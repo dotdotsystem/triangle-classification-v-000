c3eed40c1a951068e39fe42c429b22db2ee83c8e
Claire Harmer <claire.f.harmer@gmail.com>
2018-09-14 14:47:04 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+
+  def initialize (one:, two:, three:)
+    @one = one
+    @two = twp
+    @three = three
+  end
+
+  def kind
+  if @one != @two && @two != @three || @three != @one 
+    return :scalene
+  elsif @one == @two || @one == @three 
+    return :isosceles
+
+  elsif @one == @two == @three
+    return :equilateral
+  else 
+    return 
+    false
+  end 
+end 
+
+    
+    
+    
+  end 
+
+
+  
 end
